963491a599710f9cfa2dde2b487682a31567c77c
Viwe Nkepu <viwe.nkepu@hotmail.com>
2024-02-19 19:16:27 +0200

    up version

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -8,11 +8,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var baseUri = new Uri(builder.HostEnvironment.BaseAddress);
+
+var apiUri = baseUri.Scheme + Uri.SchemeDelimiter + baseUri.Host + "/:5291";
+
 builder.Services
     .AddScoped<KanBanManager>()
     .AddScoped<WebAuthenticationManager>()
 	.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("API"))
-    .AddHttpClient("API", client => client.BaseAddress = new Uri("http://localhost:5291"));
+    .AddHttpClient("API", client => client.BaseAddress = new Uri(apiUri));
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
